6acf94b19d41ad0cfdf07efb24e092e49fd12925
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-09-20 23:40:40 +0500

    itnitial commit

--- src/behaviors/SortableModel.php
+++ src/behaviors/SortableModel.php
@@ -1,7 +1,25 @@
 <?php
 namespace kilyakus\modules\behaviors;
 
-class SortableModel extends \kilyakus\behaviors\sortablemodel\SortableModel
-{
+use yii\db\ActiveRecord;
 
+class SortableModel extends \yii\base\Behavior
+{
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'findMaxOrderNum',
+        ];
+    }
+
+    public function findMaxOrderNum()
+    {
+        if(!$this->owner->order_num) {
+            $maxOrderNum = (int)(new \yii\db\Query())
+                ->select('MAX(`order_num`)')
+                ->from($this->owner->tableName())
+                ->scalar();
+            $this->owner->order_num = ++$maxOrderNum;
+        }
+    }
 }
--- src/behaviors/StatusController.php
+++ src/behaviors/StatusController.php
@@ -1,7 +1,39 @@
 <?php
 namespace kilyakus\modules\behaviors;
 
-class StatusController extends \kilyakus\behaviors\statuscontroller\StatusController
-{
+use Yii;
 
+class StatusController extends \yii\base\Behavior
+{
+    public $model;
+
+    public function changeStatus($id, $status)
+    {
+        $modelClass = $this->model;
+
+        if(($model = $modelClass::findOne($id))){
+            $model->status = $status;
+            $model->update();
+        }
+        else{
+            $this->error = Yii::t('easyii', 'Not found');
+        }
+
+        return $this->owner->formatResponse(Yii::t('easyii', 'Status successfully changed'));
+    }
+
+    public function changeHeader($id, $status)
+    {
+        $modelClass = $this->model;
+
+        if(($model = $modelClass::findOne($id))){
+            $model->header = $status;
+            $model->update();
+        }
+        else{
+            $this->error = Yii::t('easyii', 'Not found');
+        }
+
+        return $this->owner->formatResponse(Yii::t('easyii', 'Status successfully changed'));
+    }
 }
