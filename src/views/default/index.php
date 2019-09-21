d182ced5ff0c2db350324f2758d2f2ee20534ea9
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-09-21 05:58:09 +0500

    initial commit

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -3,22 +3,24 @@ use kilyakus\modules\models\Setting;
 use yii\helpers\Url;
 
 $this->title = Yii::t('easyii', 'System');
+
+$module = Yii::$app->controller->module->id;
 ?>
 
 <h4><?= Yii::t('easyii', 'Current version') ?>: <b><?= Setting::get('version') ?></b>
     <?php if(\bin\admin\AdminModule::VERSION > floatval(Setting::get('version'))) : ?>
-        <a href="<?= Url::to(['/admin/update']) ?>" class="btn btn-success"><?= Yii::t('easyii', 'Update') ?></a>
+        <a href="<?= Url::to(['/' . $module . '/update']) ?>" class="btn btn-success"><?= Yii::t('easyii', 'Update') ?></a>
     <?php endif; ?>
 </h4>
 
 <br>
 
 <p>
-    <a href="<?= Url::toRoute(['/admin/system/flush-cache']) ?>" class="btn btn-default"><i class="glyphicon glyphicon-flash"></i> <?= Yii::t('easyii', 'Flush cache') ?></a>
+    <a href="<?= Url::toRoute(['/' . $module . '/default/flush-cache']) ?>" class="btn btn-default"><i class="glyphicon glyphicon-flash"></i> <?= Yii::t('easyii', 'Flush cache') ?></a>
 </p>
 
 <br>
 
 <p>
-    <a href="<?= Url::toRoute(['/admin/system/clear-assets']) ?>" class="btn btn-default"><i class="glyphicon glyphicon-trash"></i> <?= Yii::t('easyii', 'Clear assets') ?></a>
+    <a href="<?= Url::toRoute(['/' . $module . '/default/clear-assets']) ?>" class="btn btn-default"><i class="glyphicon glyphicon-trash"></i> <?= Yii::t('easyii', 'Clear assets') ?></a>
 </p>
